78c985a40bbd171fd5a3f60063618cfd2586ce3d
Rob Paik <robpaik@gmail.com>
2019-06-17 02:21:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ function theBeatlesPlay (musicians, instruments) {
   var empty = [];
   for (var i = 0; i < musicians.length; i++) {
     var middleword = " plays"
-    var statement = console.log(musicians[i] + middleword + instruments[i])
-    empty.push(statement);
-  }
+    empty[i] = console.log(musicians[i] + middleword + instruments[i])
+    }
   return empty
 }
