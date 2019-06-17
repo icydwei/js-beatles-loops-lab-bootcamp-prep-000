33645324c35417b5ec3b34f9715f674a482f0ca2
Rob Paik <robpaik@gmail.com>
2019-06-17 01:38:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var empty = {};
+  for (i = 0; i < musicians.length, i++) {
+    console.log(`${musician[i]} plays ${instruments[i]}`)
+  }
+}
