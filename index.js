e58831de332433d7916f1f4253857d03c2f93662
Rob Paik <robpaik@gmail.com>
2019-06-17 01:44:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay (musicians, instruments) {
   var joiner = [];
-  for (i = 0; i < musicians.length; i++) {
+  for (let i = 0; i < musicians.length; i++) {
     joiner[i] = console.log(`${musicians[i]} plays ${instruments[i]}`)
   }
   return joiner
