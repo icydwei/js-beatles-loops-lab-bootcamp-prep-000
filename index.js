6ea71c78bbb25eb2a7cf10ab3e09e6cb36806a89
Rob Paik <robpaik@gmail.com>
2019-06-17 02:05:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay (musicians, instruments) {
   var empty = [];
   for (var i = 0; i < musicians.length; i++) {
-    var statement = console.log(musicians[i], " plays", instruments[i])
+    var statement = console.log(musicians[i] + " plays" + instruments[i])
     empty[i] = statement;
   }
   return empty
